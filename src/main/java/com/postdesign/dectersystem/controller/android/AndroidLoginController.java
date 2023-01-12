5fbf74582358ef1157f7ca406017813724939a2f
zhangchuqi <1290450579@qq.com>
2023-01-13 00:53:43 +0800

    实现login业务的controller层

--- src/main/java/com/postdesign/dectersystem/controller/android/AndroidLoginController.java
+++ src/main/java/com/postdesign/dectersystem/controller/android/AndroidLoginController.java
@@ -2,13 +2,28 @@ package com.postdesign.dectersystem.controller.android;
 
 
 import com.postdesign.dectersystem.service.ALoginService;
+import com.postdesign.dectersystem.utils.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/loginPage")
 public class AndroidLoginController {
 
-    ALoginService loginService;
+    @Autowired
+    private ALoginService loginService;
 
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(String uname, String psd){
+//        System.out.println(uname+": "+psd);
+        Map<String, Object> msgMap = loginService.login(uname, psd);
+        JsonUtil<Map<String, Object>> jsonUtil = new JsonUtil<>();
+//        System.out.println(jsonUtil.ObjToJson(msgMap));
+        return jsonUtil.ObjToJson(msgMap);
+    }
 }
