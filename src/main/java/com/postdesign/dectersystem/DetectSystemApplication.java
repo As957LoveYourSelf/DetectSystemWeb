82bdf5fbf620564c175a1745bded0199b374d7ea
zhangchuqi <1290450579@qq.com>
2023-01-12 14:58:47 +0800

    修改环境配置，降低SpringBoot版本

--- src/main/java/com/postdesign/dectersystem/DetectSystemApplication.java
+++ src/main/java/com/postdesign/dectersystem/DetectSystemApplication.java
@@ -1,9 +1,14 @@
 package com.postdesign.dectersystem;
 
+import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan(value = "com.postdesign.dectersystem.mapper")
+@EnableMPP
 public class DetectSystemApplication {
 
     public static void main(String[] args) {
