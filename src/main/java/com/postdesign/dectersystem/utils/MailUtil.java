1907f90eb634e0308d36564e887c389417584514
zhangchuqi <1290450579@qq.com>
2023-01-13 00:54:44 +0800

    添加了邮件发送工具类

--- src/main/java/com/postdesign/dectersystem/utils/MailUtil.java
+++ src/main/java/com/postdesign/dectersystem/utils/MailUtil.java
@@ -1,10 +1,8 @@
 package com.postdesign.dectersystem.utils;
 
-
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public final class MailUtil {
