cb33ec9a58cc0459e7a80ea36ec5b984ac2b549e
zhangchuqi <1290450579@qq.com>
2023-01-22 11:14:12 +0800

    添加了跨域和数据格式化的配置

---
+++ src/main/java/com/postdesign/detectsystem/configure/InterceptorConfig.java
@@ -0,0 +1,14 @@
+//package com.postdesign.detectsystem.configure;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+//@Configuration
+//public class InterceptorConfig extends WebMvcConfigurationSupport {
+//    @Override
+//    protected void addInterceptors(InterceptorRegistry registry) {
+//        super.addInterceptors(registry);
+//        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/**");
+//    }
+//}
