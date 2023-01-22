69bce463293aa6c195d9207f2d286c582307e084
zhangchuqi <1290450579@qq.com>
2023-01-22 11:15:20 +0800

    something change

--- src/main/java/com/postdesign/detectsystem/DetectSystemApplication.java
+++ src/main/java/com/postdesign/detectsystem/DetectSystemApplication.java
@@ -4,12 +4,12 @@ import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@MapperScan(value = "com.postdesign.dectersystem.mapper")
+@MapperScan(value = "com.postdesign.detectsystem.mapper")
 @EnableMPP
 public class DetectSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DetectSystemApplication.class, args);
     }
--- src/test/java/com/postdesign/dectersystem/DetectSystemApplicationTests.java
+++ src/test/java/com/postdesign/dectersystem/DetectSystemApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DetectSystemApplicationTests {
-
     @Test
     void contextLoads() {
     }
