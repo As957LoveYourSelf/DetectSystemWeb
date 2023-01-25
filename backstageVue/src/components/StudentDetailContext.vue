f9fa181d9fe012d3cf930e604c38ad8f13894d53
zhangchuqi <1290450579@qq.com>
2023-01-25 10:46:31 +0800

    添加学生信息详情业务

--- backstageVue/src/components/StudentDetailContext.vue
+++ backstageVue/src/components/StudentDetailContext.vue
@@ -14,21 +14,13 @@
     >
       <div>
         <el-descriptions-item label="学生姓名" v-model="data.name"></el-descriptions-item>
-
         <el-descriptions-item label="学号" v-model="data.no"></el-descriptions-item>
-
         <el-descriptions-item label="所属学院" v-model="data.collage"></el-descriptions-item>
-
         <el-descriptions-item label="专业" v-model="data.major" :span="2"></el-descriptions-item>
-
         <el-descriptions-item label="年龄" v-model="data.age" :span="2"></el-descriptions-item>
-
         <el-descriptions-item label="邮箱" v-model="data.email" :span="2"></el-descriptions-item>
-
         <el-descriptions-item label="电话" v-model="data.phone" :span="2"></el-descriptions-item>
-
         <el-descriptions-item label="年级" v-model="data.grade" :span="2"></el-descriptions-item>
-
         <el-descriptions-item label="班级" v-model="data.class" :span="2"></el-descriptions-item>
       </div>
     </el-descriptions>
