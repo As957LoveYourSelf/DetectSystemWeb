b6cce1244e7955f92cea45534ca62f15cd83f1ec
zhangchuqi <1290450579@qq.com>
2023-01-26 14:10:47 +0800

    添加教师管理请求

--- src/main/java/com/postdesign/detectsystem/controller/backstage/TeacherManageController.java
+++ src/main/java/com/postdesign/detectsystem/controller/backstage/TeacherManageController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("teacherManagePage")
+@RequestMapping("/teacherManagePage")
 public class TeacherManageController {
 
     @Autowired
---
+++ src/main/java/com/postdesign/detectsystem/utils/TokenUtil.java
@@ -0,0 +1,54 @@
+package com.postdesign.detectsystem.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenUtil {
+    private static final long EXPIRE_TIME = 15 * 60 * 1000;
+    private static final String TOKEN_SECRET = "freetimebackstageadminlogin";
+
+    /**
+     * 生成签名，15分钟过期
+     */
+    public static String getTokenSecret(String username, String password) {
+        try {
+            // 设置过期时间
+            Date date = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+            // 私钥和加密算法
+
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            // 设置头部信息
+            Map<String, Object> header = new HashMap<>(2);
+            header.put("Type", "Jwt");
+            header.put("alg", "HS256");
+            // 返回token字符串
+            return JWT.create()
+                    .withHeader(header)
+                    .withClaim("loginName", username)
+                    .withClaim("pwd", password)
+                    .withExpiresAt(date)
+                    .sign(algorithm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean verify(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT jwt = verifier.verify(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+}
